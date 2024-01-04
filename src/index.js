e80ace1adb7d0c19212e3db4cba12dd4f10724bd
mrinalsinha2211 <145749474+mrinalsinha2211@users.noreply.github.com>
2024-01-04 11:25:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
     //Write your code here
     //Don't change function name
-  
+  try{
+        await fs.access(fileName);
+        console.log(`${fileName} already exists. Skipping write operation`);
+    
+    }
+    catch{
+        try{
+            await fs.appendFile(fileContent,fileContent);
+                console.log(`File ${fileName} created and data written successfully!`);
+            
+        }
+            catch (error){
+                console.log(`Error writing file: ${error.message}`);
+            }
+    }
 };
 module.exports =  writeFile ;
